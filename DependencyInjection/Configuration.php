666b748515912b32db9f937908d17afcb0cbf26b
Annice Strömberg <annice.stromberg@carasent.com>
2026-09-18 17:38:44 +0200

    Add Symfony 5.4 configuration compatibility.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,7 +9,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('oauth2_server');
         $rootNode = $treeBuilder->root('oauth2_server');
 
         // Here you should define the parameters that are allowed to
